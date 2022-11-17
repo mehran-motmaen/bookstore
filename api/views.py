a3443bca06baf83dee229244a2dddee2cc785b99
motmaen <M5566M>
2022-11-17 23:24:05 +0330

    supprt xml

--- api/views.py
+++ api/views.py
@@ -3,17 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, status, decorators, permissions
+from rest_framework.decorators import renderer_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework_xml.renderers import XMLRenderer
 
 from api.models import Book, Author
 from api.serializers import BookSerializer, AuthorSerializer
 
 
+@renderer_classes((JSONRenderer, XMLRenderer))
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    #permission_classes = (IsAuthenticated,)
     search_fields = ('name', 'description', 'author',)
     ordering = ('name',)
     http_method_names = ['post', 'get', 'put', 'delete']
@@ -25,6 +28,7 @@ class BookViewSet(viewsets.ModelViewSet):
             return [permissions.AllowAny()]
 
         return [permissions.IsAuthenticated()]
+
     def list(self, request, *args, **kwargs):
         response = super(BookViewSet, self).list(request, *kwargs, **kwargs)
         return response
@@ -33,7 +37,8 @@ class BookViewSet(viewsets.ModelViewSet):
         try:
             user = request.user.id
             book = self.get_object()
-            if book.author.added_by == user:
+
+            if book.author.added_by.id == user:
                 response = super().destroy(request, *args, **kwargs)
 
                 return Response({'results': response.data}, status=status.HTTP_204_NO_CONTENT)
@@ -48,11 +53,11 @@ class BookViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+@renderer_classes((JSONRenderer, XMLRenderer))
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     permission_classes = (IsAuthenticated,)
     serializer_class = AuthorSerializer
-    search_fields = ('name', )
+    search_fields = ('name',)
     ordering = ('name',)
     http_method_names = ['post', 'get', 'put', 'delete']
-
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -80,6 +80,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer'
+    ],
 }
 
 
