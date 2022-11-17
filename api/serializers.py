c7c105c8bf63d7e8951bbf9a66cff703844c165b
motmaen <M5566M>
2022-11-17 18:10:32 +0330

    add serializers.py

---
+++ api/serializers.py
@@ -0,0 +1,42 @@
+import string
+
+from django.db.migrations import serializer
+from rest_framework import serializers
+
+from api.models import Book, Author
+
+
+class BookSerializer(serializer.ModelFieldSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        read_only_fields = ('created_at')
+
+    def validate_name(self, data):
+        try:
+            data.encode(encoding='utf-8').decode('ascii')
+        except UnicodeDecodeError as e:
+            raise serializers.ValidationError('set a correct name') from e
+
+        if data in string.punctuation.split():
+            raise serializers.ValidationError(
+                ('name should not contain :, ", [, ] ,white space and comma characters')
+            )
+
+
+class AuthorSerializer(serializer.ModelFieldSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+        read_only_fields = ('created_at')
+
+    def validate_name(self, data):
+        try:
+            data.encode(encoding='utf-8').decode('ascii')
+        except UnicodeDecodeError as e:
+            raise serializers.ValidationError('set a correct name') from e
+
+        if data in string.punctuation.split():
+            raise serializers.ValidationError(
+                ('name should not contain :, ", [, ] ,white space and comma characters')
+            )
